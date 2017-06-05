0965904c9e51c961dd310dbc6fe622132fc96929
punkyoon <cryptography@konkuk.ac.kr>
2017-06-05 18:35:53 +0000

    developing..

---
+++ static/js/textarea.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Form, TextArea } from 'semantic-ui-react'
+
+const textarea = () => (
+  <Form>
+    <TextArea placeholder='message' autoHeight />
+  </Form>
+)
+
+export default textarea
