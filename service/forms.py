047ec2f3412a3d85c247520ccd0b3dc4c060bf0e
punkyoon <cryptography@konkuk.ac.kr>
2017-06-27 18:47:32 +0000

    remove message form and modified main page

--- service/forms.py
+++ service/forms.py
@@ -2,16 +2,6 @@ from django import forms
 
 from service.models import Timeline
 
-class MessageForm(forms.Form):
-    message = forms.CharField(
-        widget=forms.Textarea(
-            attrs={
-                #'class': 'form-control',
-                'placeholder': 'message',
-            }
-        )
-    )
-
 class SearchForm(forms.Form):
     date = forms.DateField(
         widget=forms.SelectDateWidget(
--- service/views.py
+++ service/views.py
@@ -3,26 +3,16 @@ import datetime
 from django.shortcuts import render
 
 from service.models import Timeline
-from service.forms import MessageForm, SearchForm
+from service.forms import SearchForm
 
 def main_view(request):
-    form = MessageForm()
-
     if request.method == 'POST':
-        form = MessageForm(request.POST)
-        if form.is_valid():
-            msg = form.cleaned_data['message']
-            content = Timeline.objects.create(message=msg)
-            form = MessageForm()
+        msg = request.POST['message']
+        content = Timeline.objects.create(message=msg)
     
     result = Timeline.objects.filter(time__contains=datetime.date.today())
-    
-    context = {
-        'msg_form': form,
-        'result': result,
-    }
 
-    return render(request, 'main.html', context)
+    return render(request, 'main.html', {'result': result})
 
 def search_view(request):
     form = SearchForm()
