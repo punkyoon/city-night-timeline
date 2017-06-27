759409b26ff7896625442f03fa7f3fd84c363c6c
punkyoon <cryptography@konkuk.ac.kr>
2017-06-27 19:58:44 +0000

    remove comments

--- service/views.py
+++ service/views.py
@@ -15,28 +15,15 @@ def main_view(request):
     return render(request, 'main.html', {'result': result})
 
 def search_view(request):
-    #form = SearchForm()
-
     if request.method == 'POST':
         data = request.POST['date']
         data = datetime.datetime.strptime(data, "%B %d, %Y").date()
-        result = Timeline.objects.filter(time__contains=data)
-        return render(request, 'search.html', {'result': result})
-        '''
-        print(request.POST)
-        form = SearchForm(request.POST)
-        if form.is_valid():
-            query = form.cleaned_data['date']
-            result = Timeline.objects.filter(time__contains=query)
-            context = {
-                'search_form': form,
-                'result': result
-            }
 
-            return render(request, 'search.html', context)
-        '''
+        result = Timeline.objects.filter(time__contains=data)
+
+        return render(request, 'search.html', {'result': result})
+
     return render(request, 'search.html')
-    #return render(request, 'search.html', {'search_form': form})
 
 def help_view(request):
     return render(request, 'help.html')
